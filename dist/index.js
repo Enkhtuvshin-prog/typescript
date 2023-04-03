a7e622f9960904341d6b726425d2e2bb0c299fda
Enkhtvshn <Enkhtuwshin.1201@gmail.com>
2023-04-03 19:55:38 +0800

    first commit

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const ner = "Pinecone";
+let age = 22;
+console.log("name:age", ner, age);
+function sum(a, b) {
+    return a + b;
+}
+const niilber = sum(7, 9);
+console.log(niilber);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const ner: string = "Pinecone";
+let age: number = 22;
+console.log("name:age", ner, age);
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+const niilber = sum(7, 9);
+console.log(niilber);
